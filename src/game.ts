dc9c39282154966d8d58feab1999711338984de3
Matthew Nelson <menelso2@illinois.edu>
2021-06-05 13:00:22 -0700

    Refactor game for readability

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,3 @@
-import { replaceItemAtIndex } from "./utils";
-
 export type Position = [number, number];
 
 export type Move = [Position, Position];
@@ -48,24 +46,36 @@ function fail(message: string): AttemptFailure {
     };
 }
 
+function movePiece(
+    board: Piece[],
+    activePiece: Piece,
+    position: Position
+): Board {
+    const index = board.findIndex((piece) => piece == activePiece);
+
+    if (index == -1) {
+        throw new Error(
+            "Attempting to move a piece that doesn't exist on the board"
+        );
+    }
+
+    const newPiece = { ...activePiece, position };
+
+    return [...board.slice(0, index), newPiece, ...board.slice(index + 1)];
+}
+
 // assume that move starts at a valid position (with correct color piece) on the board and validate that before this point in program
 export function attemptMove(board: Board, move: Move): AttemptResult {
     const [start, end] = move;
-    const activePieceIndex = board.findIndex(
-        (piece) => piece.position == start
-    );
-    if (activePieceIndex == -1) {
+    const activePiece = board.find((piece) => piece.position == start);
+
+    if (activePiece == undefined) {
         return fail(
             "Invalid start for move: piece does not exist in that location"
         );
     }
-    const activePieceStart = board[activePieceIndex];
-    const activePieceEnd = { ...activePieceStart, position: end };
-    const resultingBoard = replaceItemAtIndex(
-        board,
-        activePieceIndex,
-        activePieceEnd
-    );
+
+    const resultingBoard = movePiece(board, activePiece, end);
 
     return succeed(resultingBoard);
 }
