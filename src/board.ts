8c521ae2fbe3b302a18ad7a3fd75066fab3e18f9
Matthew Nelson <menelso2@illinois.edu>
2021-06-06 21:35:43 -0700

    Implement new board creation

--- src/board.ts
+++ src/board.ts
@@ -1,3 +1,5 @@
+/* type declarations */
+
 export type Position = [number, number];
 
 export enum PieceType {
@@ -15,6 +17,8 @@ export interface Piece {
 }
 export type Board = Piece[];
 
+/* core module exports */
+
 export function makePiece(
     color: Color,
     position: Position,
@@ -25,4 +29,23 @@ export function makePiece(
         type,
         position,
     };
+}
+
+export function makeNewBoard() {
+    const boardSize = 8;
+    const board: Board = [];
+
+    for (let i = 0; i < boardSize; i++) {
+        if (i % 2 != 0) {
+            board.push(makePiece(Color.Black, [0, i])); // row 0
+            board.push(makePiece(Color.Black, [2, i])); // row 2
+            board.push(makePiece(Color.Red, [6, i])); // row 6
+        } else {
+            board.push(makePiece(Color.Black, [1, i])); // row 2
+            board.push(makePiece(Color.Red, [5, i])); // row 5
+            board.push(makePiece(Color.Red, [7, i])); // row 7
+        }
+    }
+
+    return board;
 }
---
+++ test/board.test.ts
@@ -0,0 +1,19 @@
+import { Color, makeNewBoard } from "../src/board";
+
+describe("Board creation", () => {
+    test("A new board has 24 pieces", () => {
+        expect(makeNewBoard().length).toBe(24);
+    });
+
+    test("Each color has 12 pieces on a new board", () => {
+        const board = makeNewBoard();
+        const black = board.filter((piece) => piece.color == Color.Black);
+        expect(black.length).toBe(12); // since only two colors, red must also have 12
+    });
+
+    test("Each color has 12 pieces on a new board", () => {
+        const board = makeNewBoard();
+        const black = board.filter((piece) => piece.color == Color.Black);
+        expect(black.length).toBe(12); // since only two colors, red must also have 12
+    });
+});
