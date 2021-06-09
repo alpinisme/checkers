94e316273477c23130321b56a3949891a78e5cee
Matthew Nelson <menelso2@illinois.edu>
2021-06-09 09:57:51 -0700

    Fix file name type

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import authenticationRouter from "./authenticationRoutes";
+import authenticationRouter from "./authenticationRouter";
 
 const router = Router();
 
