11738a6cc0cd75e6944e03e7f9521b5ac34608ba
Matthew Nelson <menelso2@illinois.edu>
2021-06-09 11:23:41 -0700

    Create mock app for tests with auth user

---
+++ test/integration/authenticatedMockApp.ts
@@ -0,0 +1,23 @@
+import app from "../../src/server";
+import express from "express";
+import session from "express-session";
+import { User } from "../../src/store/userStore";
+
+const mockAuthUser: User = {
+    username: "Joe Shmo",
+    losses: 0,
+    wins: 42,
+};
+
+const mockApp = express();
+
+mockApp.use(session({ secret: "hey" }));
+
+mockApp.all("*", function (req, res, next) {
+    req.session.user = mockAuthUser;
+    next();
+});
+
+mockApp.use(app);
+
+export { mockApp, mockAuthUser };
