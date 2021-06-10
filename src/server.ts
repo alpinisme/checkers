d49528a3e31a3a8bd8599f03adb70b5739378363
Matthew Nelson <menelso2@illinois.edu>
2021-06-09 21:04:42 -0700

    Rename the app

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import config from "config";
 const port = config.get("app.port");
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Checkers app listening at http://localhost:${port}`);
 });
