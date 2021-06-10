ca9c36a34e2735306d7277b6eaa560597cc5127c
Matthew Nelson <menelso2@illinois.edu>
2021-06-09 19:36:40 -0700

    Move logic from auth router to controller

--- src/routes/authenticationRouter.ts
+++ src/routes/authenticationRouter.ts
@@ -1,42 +1,21 @@
 import { Router } from "express";
-import ValidationError from "../errors/ValidationError";
-import userStore from "../store/userStore";
 import { validateLogin, validateRegistration } from "../middleware/validations";
+import authenticationController from "../controllers/authenticationController";
 
 const authenticationRouter = Router();
 
-authenticationRouter.post("/login", validateLogin, async (req, res) => {
-    const { username, password } = req.body;
-    const authenticated = await userStore.checkPassword(username, password);
-    if (!authenticated) {
-        res.status(401).end();
-    }
-    req.session.user = await userStore.get(username);
-    res.status(204).end();
-});
+authenticationRouter.post(
+    "/login",
+    validateLogin,
+    authenticationController.login
+);
 
 authenticationRouter.post(
     "/register",
     validateRegistration,
-    async (req, res) => {
-        const { username, password } = req.body;
-        try {
-            await userStore.create(username, password);
-            req.session.user = await userStore.get(username);
-            res.status(204).end();
-        } catch (error) {
-            if (error instanceof ValidationError) {
-                res.status(400).send({ errors: { [error.field]: error.rule } });
-            } else {
-                throw error;
-            }
-        }
-    }
+    authenticationController.register
 );
 
-authenticationRouter.post("/logout", (req, res) => {
-    delete req.session.user;
-    res.sendStatus(200);
-});
+authenticationRouter.post("/logout", authenticationController.logout);
 
 export default authenticationRouter;
