ea0a2113e8d20e1065c4ffc8848137d3dd7b943b
Matthew Nelson <menelso2@illinois.edu>
2021-06-10 10:42:19 -0700

    Split store tests into separate files

--- test/unit/store/userStore.test.ts
+++ test/unit/store/userStore.test.ts
@@ -1,56 +1,11 @@
-import gameStore from "../../src/store/gameStore";
-import userStore from "../../src/store/userStore";
-import chatStore from "../../src/store/chatStore";
-import redis from "../../src/store/redis";
-import { makeNewGame } from "../../src/models/game";
-import { Message } from "../../src/models/chat";
+import userStore from "../../../src/store/userStore";
+import chatStore from "../../../src/store/chatStore";
+import redis from "../../../src/store/redis";
+import { Message } from "../../../src/models/chat";
 
 jest.mock("ioredis", () => require("ioredis-mock/jest"));
 
 beforeEach(() => redis.flushdb());
-
-describe("Game store", () => {
-    test("A game can be saved and retrieved", async () => {
-        const player1 = "alpha";
-        const player2 = "omega";
-        const gameIdPrefix = player1 + ":" + player2;
-        gameStore.create(makeNewGame(player1, player2));
-        const ids = await gameStore.allIdsBelongingTo(player1);
-        const id = ids[0];
-        expect(id.startsWith(gameIdPrefix)).toBe(true);
-    });
-
-    test("Updating a game should increase its score in the set of all the player's games (and only that player's)", async () => {
-        const player1 = "alpha";
-        const player2 = "omega";
-        const game = makeNewGame(player1, player2);
-        gameStore.create(game);
-        const initial = await gameStore.allIdsBelongingTo(player1);
-        await new Promise((resolve) => setTimeout(() => resolve(true), 2));
-        gameStore.update(player1, initial[0], game);
-        const final = await gameStore.allIdsBelongingTo(player1);
-        const player2Initial = await gameStore.allIdsBelongingTo(player2);
-        const player2Final = await gameStore.allIdsBelongingTo(player2);
-
-        expect(Number(initial[1])).toBeLessThan(Number(final[1]));
-        expect(Number(player2Initial[1])).not.toBeLessThan(
-            Number(player2Final[1])
-        );
-    });
-
-    test("A non-game stored as a game should throw an error when retrieved", async () => {
-        expect.assertions(1);
-        const player1 = "alpha";
-        const player2 = "omega";
-        const game: any = {};
-        gameStore.create(game);
-        const ids = await gameStore.allIdsBelongingTo(player1);
-
-        await expect(gameStore.get("game:" + ids[0])).rejects.toThrowError(
-            "Failed to parse non-string as json"
-        );
-    });
-});
 
 describe("User store", () => {
     test("Password check works", async () => {
